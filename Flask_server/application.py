f72f7da251c2653286ddeefeed258a4c3aad55c9
Taha <91777330+tahamukhtar20@users.noreply.github.com>
2022-12-18 04:54:58 +0500

    pushing files

--- Flask_server/application.py
+++ Flask_server/application.py
@@ -1,38 +1,71 @@
-import datetime
+# cython: language_level=3
+from collections import defaultdict, Counter
+# from concurrent.futures import ProcessPoolExecutor
+import json
 import os
-from Inverted_Indexing import inverted_index
+import datetime
+from nltk.corpus import stopwords
+from nltk.tokenize import WordPunctTokenizer
+from nltk.stem.snowball import SnowballStemmer
+import pandas as pd
+import re
+
+punctuations = re.compile(r'[^\w\s]')
+stop_words = set(stopwords.words('english'))
+tokenizer = WordPunctTokenizer()
+stemmer = SnowballStemmer('english')
 
 
-def create_inverted_index():
-    # dataset = os.listdir(path)
-    # with multiprocessing.Pool(processes=6) as p:
-    #     __inverted_list__ = p.map(inverted_index, [(path + x) for x in dataset])
-    # return __inverted_list__
-    dataset = os.listdir(path)
-    __inverted_list__ = []
-    for x in dataset:
-        __inverted_list__.append(inverted_index(path + x))
-    return __inverted_list__
+def json_parser(path1):
+    def temp_parse(x):
+        # Tokenize and stem the input string
+        x = tokenizer.tokenize(punctuations.sub('', x))
+        x = [stemmer.stem(word) for word in x if word.lower() not in stop_words]
+        return x
+
+    with open(path1, 'r') as f:
+        df = pd.DataFrame(json.load(f))
+    df['content'] = df['content'].apply(temp_parse)
+    unique_tokens = set(token for doc in df['content'].values for token in doc)
+    doc_dict = {row['id']: row['content'] for row in df.to_dict(orient='records')}
+    return unique_tokens, doc_dict
 
 
-def merge_dict(merge_list):
-    merged_dict = {}
-    for item in merge_list:
-        for outer_key, outer_value in item.items():
-            if outer_key not in merged_dict:
-                merged_dict[outer_key] = outer_value
-                continue
-            for inner_key, inner_value in outer_value.items():
-                if inner_key not in merged_dict[outer_key]:
-                    merged_dict[outer_key][inner_key] = inner_value
-                    continue
-                merged_dict[outer_key][inner_key] += inner_value
-    return merged_dict
+# def create_inverted_index(path3):
+#     with ProcessPoolExecutor() as executor:
+#         indexes = list(executor.map(inverted_index, (path3 + x for x in os.listdir(path3))))
+#     merged_index = defaultdict(Counter)
+#     for index in indexes:
+#         for token, doc_counts in index.items():
+#             merged_index[token] += doc_counts
+#     return merged_index
+def inverted_index(path2):
+    unique_tokens, doc_dict = json_parser(path2)
+    inverted_indexing = {}
+    for doc_id, tokens in doc_dict.items():
+        for token in tokens:
+            if token not in inverted_indexing:
+                inverted_indexing[token] = {}
+            if doc_id in inverted_indexing[token]:
+                inverted_indexing[token][doc_id] += 1
+            else:
+                inverted_indexing[token][doc_id] = 1
+    return inverted_indexing
+
+
+def create_inverted_index(path):
+    merged_index = defaultdict(Counter)
+    for file in os.listdir(path):
+        index = inverted_index(path + file)
+        for token, doc_counts in index.items():
+            merged_index[token] += doc_counts
+    return merged_index
 
 
 if __name__ == '__main__':
-    # 'X:\\Dataset\\nela-gt-2021\\newsdata\\'
     path = ".\\temp_testing\\"
     x1 = datetime.datetime.now()
-    open('.\\output_test.json', 'w', encoding='utf-8').write(str(merge_dict(create_inverted_index())))
+    index = create_inverted_index(path)
+    with open('.\\output_test.json', 'w', encoding='utf-8') as fx:
+        json.dump(index, fx)
     print(f"Time taken: {datetime.datetime.now() - x1}")
