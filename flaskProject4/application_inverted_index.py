25e5b7847d8af4f631639bfa53f0608f5d5c5717
Shalina Riaz <108185986+shalinaariaaz@users.noreply.github.com>
2022-12-30 01:31:54 +0500

    Last Commit

--- flaskProject4/application_inverted_index.py
+++ flaskProject4/application_inverted_index.py
@@ -58,3 +58,57 @@ def inverted_index(path2):
         for word, pos_list in tokens.items():
             inverted_indexing[word][doc_id] = pos_list
     return inverted_indexing
+
+def create_inverted_index(path3):
+    indexes = []
+    if len(os.listdir(path3)) < 6:
+        for file in os.listdir(path3):
+            indexes.append(inverted_index(path3 + file))
+    else:
+        with ProcessPoolExecutor() as executor:
+            indexes = list(executor.map(inverted_index, (path3 + x for x in os.listdir(path3))))
+        print("Inverted Indexing Done")
+    merged_index = {}
+    for inv_index in indexes:
+        for token, docs_ in inv_index.items():
+            if token not in merged_index:
+                merged_index[token] = {}
+            merged_index[token].update(docs_)
+    return merged_index
+
+
+def articles_count(path3):
+    with ProcessPoolExecutor() as executor:
+        _templist_ = list(executor.map(read_file, (path3 + x for x in os.listdir(path))))
+        _noofdocs = sum(templist_)
+    print("Total Number of Documents: ", _noof_docs)
+    return _noof_docs
+
+
+def read_file(path):
+    with open(path, "r", encoding="utf-8") as f1:
+        no_of_docs = len(json.load(f1))
+    return no_of_docs
+
+
+def _idf_index(doc_dict, no_of_docs):
+    temp_ = no_of_docs / (len(doc_dict) + 1)
+    temp1 = math.log(temp_)
+    for key_, value_ in doc_dict.items():
+        doc_dict[key_] = value_ * temp1
+    doc_dict = OrderedDict(sorted(doc_dict.items(), key=lambda x: x[1], reverse=True))
+    return doc_dict
+
+
+if _name_ == '_main_':
+    path = ".\\minidataset\\"
+    x1 = datetime.datetime.now()
+    index = create_inverted_index(path)
+    no_of_articles = articles_count(path)
+    for key, value in index.items():
+        index[key] = _idf_index(value, no_of_articles)
+    final_index = (no_of_articles, index)
+    print("Inverted Indexing Done")
+    with open('.\\output_test.json', 'w', encoding='utf-8') as fx:
+        json.dump(final_index, fx)
+    print(f"Time taken: {datetime.datetime.now() - x1}")
